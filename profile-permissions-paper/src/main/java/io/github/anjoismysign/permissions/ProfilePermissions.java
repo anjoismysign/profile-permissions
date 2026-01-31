0fcf34e8be5e88064552748953eae25b0e39de6b
anjoismysign <lubensaico@gmail.com>
2026-01-31 12:59:13 -0600

    Add /profilepermissions command, AccountCruder, refactor account/profile

--- profile-permissions-paper/src/main/java/io/github/anjoismysign/permissions/ProfilePermissions.java
+++ profile-permissions-paper/src/main/java/io/github/anjoismysign/permissions/ProfilePermissions.java
@@ -1,21 +1,35 @@
 package io.github.anjoismysign.permissions;
 
+import io.github.anjoismysign.bloblib.api.BlobLibMessageAPI;
 import io.github.anjoismysign.bloblib.managers.BlobPlugin;
-import io.github.anjoismysign.bloblib.managers.cruder.ProfileCruder;
+import io.github.anjoismysign.bloblib.managers.cruder.AccountCruder;
 import io.github.anjoismysign.permissions.director.PermissionsManagerDirector;
 import io.github.anjoismysign.permissions.entity.PermissionsAccount;
 import io.github.anjoismysign.permissions.entity.PermissionsProfile;
 import io.github.anjoismysign.permissions.entity.ProfilePermissionsAccount;
+import io.github.anjoismysign.skeramidcommands.command.Command;
+import io.github.anjoismysign.skeramidcommands.command.CommandTarget;
+import io.github.anjoismysign.skeramidcommands.commandtarget.BukkitCommandTarget;
+import io.github.anjoismysign.skeramidcommands.commandtarget.LogicCommandParameters;
+import io.github.anjoismysign.skeramidcommands.server.bukkit.BukkitAdapter;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public final class ProfilePermissions extends BlobPlugin {
 
     private static ProfilePermissions INSTANCE;
 
     private PermissionsManagerDirector director;
-    private ProfileCruder<PermissionsAccount, PermissionsProfile> profileCruder;
+    private AccountCruder<PermissionsAccount, PermissionsProfile> accountCruder;
 
     public static ProfilePermissions getInstance() {
         return INSTANCE;
@@ -26,14 +40,67 @@ public final class ProfilePermissions extends BlobPlugin {
         INSTANCE = this;
         director = new PermissionsManagerDirector(this);
         Bukkit.getScheduler().runTask(this, () -> {
-            profileCruder = new ProfileCruder<>(this, PermissionsAccount.class, PermissionsAccount::new);
+            accountCruder = new AccountCruder<>(this, PermissionsAccount.class, PermissionsProfile.class);
         });
+        Command mainCommand = BukkitAdapter.getInstance().ofBukkitCommand("profilepermissions");
+        Command setCommand = mainCommand.child("set");
+        var onlinePlayers = BukkitCommandTarget.ONLINE_PLAYERS();
+        var bool = LogicCommandParameters.BOOLEAN();
+        setCommand.setParameters(
+                onlinePlayers,
+                new CommandTarget<String>() {
+                    private void resolveRecursive(Permission perm, Set<String> accumulator) {
+                        if (!accumulator.add(perm.getName())) {
+                            return;
+                        }
+                        perm.getChildren().keySet().forEach(childName -> {
+                            Permission childPerm = Bukkit.getServer().getPluginManager().getPermission(childName);
+                            if (childPerm != null) {
+                                resolveRecursive(childPerm, accumulator);
+                            } else {
+                                accumulator.add(childName);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public List<String> get() {
+                        Set<String> allNodes = new HashSet<>();
+                        for (Permission perm : Bukkit.getServer().getPluginManager().getPermissions()) {
+                            resolveRecursive(perm, allNodes);
+                        }
+                        return new ArrayList<>(allNodes);
+                    }
+
+                    @Override
+                    public @Nullable String parse(String s) {
+                        return s;
+                    }
+                },
+                bool);
+        setCommand.onExecute(((permissionMessenger, args) -> {
+            CommandSender sender = BukkitAdapter.getInstance().of(permissionMessenger);
+            Player player = onlinePlayers.parse(args[0]);
+            if (player == null) {
+                BlobLibMessageAPI.getInstance()
+                        .getMessage("Player.Not-Found", sender)
+                        .toCommandSender(sender);
+                return;
+            }
+            String permission = args[1];
+            boolean value = Boolean.TRUE.equals(bool.parse(args[2]));
+            var account = getAccount(player);
+            if (account == null){
+                return;
+            }
+            account.setPermission(permission, value);
+        }));
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
-        profileCruder.shutdown();
+        accountCruder.shutdown();
     }
 
     @NotNull
@@ -42,12 +109,12 @@ public final class ProfilePermissions extends BlobPlugin {
     }
 
     @NotNull
-    public ProfileCruder<PermissionsAccount, PermissionsProfile> getProfileCruder() {
-        return profileCruder;
+    public AccountCruder<PermissionsAccount, PermissionsProfile> getAccountCruder() {
+        return accountCruder;
     }
 
-    @NotNull
-    public ProfilePermissionsAccount getAccount(@NotNull Player player){
-        return profileCruder.getAccount(player);
+    @Nullable
+    public ProfilePermissionsAccount getAccount(@NotNull Player player) {
+        return accountCruder.getAccount(player);
     }
 }
--- profile-permissions-paper/src/main/java/io/github/anjoismysign/permissions/entity/PermissionsProfile.java
+++ profile-permissions-paper/src/main/java/io/github/anjoismysign/permissions/entity/PermissionsProfile.java
@@ -1,9 +1,96 @@
 package io.github.anjoismysign.permissions.entity;
 
+import com.google.common.collect.Maps;
+import io.github.anjoismysign.bloblib.entities.Cleanable;
+import io.github.anjoismysign.bloblib.entities.PlayerDecorator;
+import io.github.anjoismysign.bloblib.entities.PlayerDecoratorAware;
+import io.github.anjoismysign.permissions.ProfilePermissions;
 import io.github.anjoismysign.psa.crud.Crudable;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachment;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
-public record PermissionsProfile(String getIdentification,
-                                 Map<String, Boolean> getPermissions) implements Crudable {
+public final class PermissionsProfile implements Crudable, Cleanable, PlayerDecoratorAware, ProfilePermissionsAccount {
+    private final String identification;
+    private final Map<String, Boolean> permissions;
+    private transient PlayerDecorator playerDecorator;
+    private transient PermissionAttachment attachment;
+
+    public PermissionsProfile(String identification) {
+        this.identification = identification;
+        this.permissions = Maps.newHashMap();
+    }
+
+    @Override
+    public void setPermission(String permission, boolean value) {
+        permissions.put(permission, value);
+        if (attachment == null) {
+            return;
+        }
+        attachment.setPermission(permission, value);
+    }
+
+    @Override
+    public void unsetPermission(String permission) {
+        permissions.remove(permission);
+        if (attachment == null) {
+            return;
+        }
+        attachment.unsetPermission(permission);
+    }
+
+    @Override
+    public String getIdentification() {
+        return identification;
+    }
+
+    @Override
+    public void cleanup() {
+        Runnable syncRunnable = () -> {
+            if (!playerDecorator.isValid()){
+                return;
+            }
+            @Nullable var player = player();
+            if (player != null){
+                attachment = player.addAttachment(ProfilePermissions.getInstance());
+            }
+        };
+        if (Bukkit.isPrimaryThread()){
+            syncRunnable.run();
+        } else {
+            Bukkit.getScheduler().runTask(ProfilePermissions.getInstance(), syncRunnable);
+        }
+    }
+
+    @Override
+    public void setPlayerDecorator(@NotNull PlayerDecorator playerDecorator) {
+        Runnable syncRunnable = () -> {
+          if (!playerDecorator.isValid()){
+              return;
+          }
+            @Nullable var player = player();
+            if (player != null){
+                attachment = player.addAttachment(ProfilePermissions.getInstance());
+                permissions.forEach(attachment::setPermission);
+            }
+        };
+        if (Bukkit.isPrimaryThread()){
+            syncRunnable.run();
+        } else {
+            Bukkit.getScheduler().runTask(ProfilePermissions.getInstance(), syncRunnable);
+        }
+        if (this.playerDecorator != null){
+            return;
+        }
+        this.playerDecorator = playerDecorator;
+    }
+
+    @Nullable
+    public Player player(){
+        return playerDecorator == null ? null : playerDecorator.lookup();
+    }
 }
